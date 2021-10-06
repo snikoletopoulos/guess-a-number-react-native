eb54dec95df2c755173a1d9ca6c0e5148ebe2364
Stavros <snikoletopoulos@hotmail.com>
2021-10-06 22:59:13 +0300

    Add general style to StartGameScreen

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 const StartGameScreen = () => {
 	return (
 		<View style={styles.screen}>
-			<Text>Test</Text>
+			<Text style={styles.title}>Start new game</Text>
+			<View style={styles.inputContainer}>
+				<Text>Select a number</Text>
+				<TextInput />
+				<View style={styles.buttonContainer}>
+					<Button title="Reset" onPress={() => {}} />
+					<Button title="Confirm" onPress={() => {}} />
+				</View>
+			</View>
 		</View>
 	);
 };
@@ -15,6 +23,21 @@ const styles = StyleSheet.create({
 		padding: 10,
 		alignItems: "center",
 	},
+	title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+	inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+	buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 10
+  },
 });
 
 export default StartGameScreen;
