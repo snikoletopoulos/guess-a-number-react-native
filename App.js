6a8ae3a57e2a934d26e4f1c039426d136951c86e
Stavros <snikoletopoulos@hotmail.com>
2021-10-09 18:50:57 +0300

    Format

--- App.js
+++ App.js
@@ -15,59 +15,59 @@ const fetchFonts = () => {
 };
 
 export default function App() {
-	const [userNumber, setUserNumber] = useState(null);
-	const [rounds, setRounds] = useState(0);
-	const [dataLoaded, setDataLoaded] = useState(false);
+  const [userNumber, setUserNumber] = useState(null);
+  const [rounds, setRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-	if (!dataLoaded) {
-		return (
-			<AppLoading
-				startAsync={fetchFonts}
-				onFinish={() => setDataLoaded(true)}
-				onError={err => console.log(err)}
-			/>
-		);
-	}
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={err => console.log(err, "hiiiiiiiiii")}
+      />
+    );
+  }
 
-	const configureNewGameHandler = () => {
-		setRounds(0);
-		setUserNumber(null);
-	};
+  const configureNewGameHandler = () => {
+    setRounds(0);
+    setUserNumber(null);
+  };
 
-	const startGameHandler = selectedNumber => {
-		setUserNumber(selectedNumber);
-	};
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
 
-	const gameOverHandler = numOfRounds => {
-		setRounds(numOfRounds);
-	};
+  const gameOverHandler = numOfRounds => {
+    setRounds(numOfRounds);
+  };
 
-	let content = <StartGameScreen onStartGame={startGameHandler} />;
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-	if (userNumber && rounds <= 0) {
-		content = (
-			<GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-		);
-	} else if (rounds > 0) {
-		content = (
-			<GameOverScreen
-				roundsNumber={rounds}
-				userNumber={userNumber}
-				onRestart={configureNewGameHandler}
-			/>
-		);
-	}
+  if (userNumber && rounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (rounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={rounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
+  }
 
-	return (
-		<View style={styles.screen}>
-			<Header title="Guess a Number" />
-			{content}
-		</View>
-	);
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      {content}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-	},
+  screen: {
+    flex: 1,
+  },
 });
