22c3105b13d879bf99f58fe8a02910bd42bf5874
Stavros <snikoletopoulos@hotmail.com>
2021-10-09 18:56:09 +0300

    Resolve merge

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import * as Font from "expo-font";
 import { AppLoading } from "expo";
 
 const fetchFonts = () => {
-<<<<<<< HEAD
   return Font.loadAsync({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
@@ -65,64 +64,6 @@ export default function App() {
       {content}
     </View>
   );
-=======
-	return Font.loadAsync({
-		"open-sans": require("./assets/fonts/OpensSans-Regular.ttf"),
-		"open-sans-bold": require("./assets/fonts/OpensSans-Bold.ttf"),
-	});
-};
-
-export default function App() {
-	const [userNumber, setUserNumber] = useState(null);
-	const [rounds, setRounds] = useState(0);
-	const [dataLoaded, setDataLoaded] = useState(false);
-
-	if (!dataLoaded) {
-		return (
-			<AppLoading
-				startAsync={fetchFonts}
-				onFinish={() => setDataLoaded(true)}
-				onError={err => console.log(err)}
-			/>
-		);
-	}
-
-	const configureNewGameHandler = () => {
-		setRounds(0);
-		setUserNumber(null);
-	};
-
-	const startGameHandler = selectedNumber => {
-		setUserNumber(selectedNumber);
-	};
-
-	const gameOverHandler = numOfRounds => {
-		setRounds(numOfRounds);
-	};
-
-	let content = <StartGameScreen onStartGame={startGameHandler} />;
-
-	if (userNumber && rounds <= 0) {
-		content = (
-			<GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-		);
-	} else if (rounds > 0) {
-		content = (
-			<GameOverScreen
-				roundsNumber={rounds}
-				userNumber={userNumber}
-				onRestart={configureNewGameHandler}
-			/>
-		);
-	}
-
-	return (
-		<View style={styles.screen}>
-			<Header title="Guess a Number" />
-			{content}
-		</View>
-	);
->>>>>>> main
 }
 
 const styles = StyleSheet.create({
