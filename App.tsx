2b90415d868e5ebb92e516970c83585fd9ceeaf1
Stavros <snikoletopoulos@hotmail.com>
2022-01-29 14:04:50 +0200

    Convert to absolute paths

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import Header from "./components/Header";
+import Header from "components/Header";
 import StartGameScreen from "screens/StartGameScreen";
-import GameScreen from "./screens/GameScreen";
-import GameOverScreen from "./screens/GameOverScreen";
+import GameScreen from "screens/GameScreen";
+import GameOverScreen from "screens/GameOverScreen";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
