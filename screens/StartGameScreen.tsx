7be8e173ab5952fdb7812d7719ef9db6c262fe83
Stavros <snikoletopoulos@hotmail.com>
2022-02-05 00:25:58 +0200

    Change the layout depending on device properties

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   ViewStyle,
@@ -7,7 +7,9 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
-  Dimensions
+  Dimensions,
+  ScrollView,
+  KeyboardAvoidingView,
 } from "react-native";
 import colors from "constants/colors";
 
@@ -25,6 +27,17 @@ const StartGameScreen: React.FC<Props> = props => {
   const [enteredValue, setEnteredValue] = useState("");
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState(9);
+  const [buttonWidth, setButtonWidth] = useState(
+    Dimensions.get("window").width / 4
+  );
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", () => {
+      setButtonWidth(Dimensions.get("window").width / 4);
+    });
+
+    return () => subscription.remove();
+  }, []);
 
   const numberInputHandler = (inputValue: string) => {
     setEnteredValue(inputValue.replace(/[^0-9]/g, ""));
@@ -63,44 +76,48 @@ const StartGameScreen: React.FC<Props> = props => {
   };
 
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}
-    >
-      <View style={styles.screen}>
-        <BodyText style={styles.title}>Start new game</BodyText>
-        <Card style={styles.inputContainer}>
-          <BodyText>Select a number</BodyText>
-          <Input
-            value={enteredValue}
-            autoCorrect={false}
-            blurOnSubmit
-            keyboardType="number-pad"
-            maxLength={2}
-            style={styles.input}
-            onChangeText={numberInputHandler}
-          />
-          <View style={styles.buttonContainer}>
-            <View style={styles.button}>
-              <Button
-                title="Reset"
-                color={colors.accent}
-                onPress={resetInputHandler}
+    <ScrollView>
+      <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={30}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            Keyboard.dismiss();
+          }}
+        >
+          <View style={styles.screen}>
+            <BodyText style={styles.title}>Start new game</BodyText>
+            <Card style={styles.inputContainer}>
+              <BodyText>Select a number</BodyText>
+              <Input
+                value={enteredValue}
+                autoCorrect={false}
+                blurOnSubmit
+                keyboardType="number-pad"
+                maxLength={2}
+                style={styles.input}
+                onChangeText={numberInputHandler}
               />
-            </View>
-            <View style={styles.button}>
-              <Button
-                title="Confirm"
-                color={colors.primary}
-                onPress={confirmInputHandler}
-              />
-            </View>
+              <View style={styles.buttonContainer}>
+                <View style={{ width: buttonWidth }}>
+                  <Button
+                    title="Reset"
+                    color={colors.accent}
+                    onPress={resetInputHandler}
+                  />
+                </View>
+                <View style={{ width: buttonWidth }}>
+                  <Button
+                    title="Confirm"
+                    color={colors.primary}
+                    onPress={confirmInputHandler}
+                  />
+                </View>
+              </View>
+            </Card>
+            {confirmedOutput}
           </View>
-        </Card>
-        {confirmedOutput}
-      </View>
-    </TouchableWithoutFeedback>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
@@ -108,7 +125,6 @@ export default StartGameScreen;
 
 interface Styles {
   screen: ViewStyle;
-
 }
 
 const styles = StyleSheet.create({
